cfa8100261e4ced131de77fa3e0a6f312029ff41
unknown <ndson1703@gmail.com>
2023-06-05 01:12:27 +0700

    btvn2905

--- BTVN_2905/http/server.c
+++ BTVN_2905/http/server.c
@@ -1,71 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <unistd.h>
-#include <netdb.h>
-#include <arpa/inet.h>
 #include <string.h>
-#include <signal.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <sys/wait.h>
 
-int main() 
+#define NUM_CHILDREN 4 // Số lượng tiến trình con
+
+void handle_client(int client)
 {
-    int listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (listener == -1)
+    // Nhận dữ liệu từ client và in ra màn hình
+    char buf[256];
+    int ret = recv(client, buf, sizeof(buf), 0);
+    buf[ret] = '\0';
+    printf("%s\n", buf);
+
+    // Trả lại kết quả cho client
+    char *msg = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Xin chao cac ban</h1></body></html>";
+    send(client, msg, strlen(msg), 0);
+
+    // Đóng kết nối
+    close(client);
+}
+
+int main()
+{
+    int listener;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+    socklen_t client_len;
+    int i;
+
+    // Khởi tạo socket
+    listener = socket(AF_INET, SOCK_STREAM, 0);
+    if (listener < 0)
     {
-        perror("socket() failed");
-        return 1;
+        perror("Error creating socket");
+        exit(1);
     }
 
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = htons(9000);
+    // Cấu hình địa chỉ server
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(9000);
 
-    if (bind(listener, (struct sockaddr *)&addr, sizeof(addr))) 
+    // Gắn địa chỉ server vào socket
+    if (bind(listener, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
-        perror("bind() failed");
-        return 1;
+        perror("Error binding");
+        exit(1);
     }
 
-    if (listen(listener, 5)) 
+    // Lắng nghe kết nối
+    if (listen(listener, 5) < 0)
     {
-        perror("listen() failed");
-        return 1;
+        perror("Error listening");
+        exit(1);
     }
 
-    int num_processes = 8;
-    for (int i = 0; i < num_processes; i++)
-        if (fork() == 0)
+    // Tạo tiến trình con
+    for (i = 0; i < NUM_CHILDREN; i++)
+    {
+        pid_t pid = fork();
+
+        if (pid == 0)
         {
-            char buf[256];
+            // Tiến trình con xử lý client
             while (1)
             {
-                // chờ kết nối
-                int client = accept(listener, NULL, NULL);
+                // Chờ kết nối mới
+                int client = accept(listener, (struct sockaddr *)&client_addr, &client_len);
                 printf("New client connected: %d\n", client);
-                // chờ dữ liệu từ client
-                int ret = recv(client, buf, sizeof(buf), 0);
-                if (ret <= 0)
-                {
-                    close(client);
-                    continue;
-                }
-                
-                buf[ret] = 0;
-                printf("Received: %s\n", buf);
-                // Trả lại kết quả cho client
-                char *msg = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Xin chao cac ban</h1></body></html>";
-                send(client, msg, strlen(msg), 0);
-                close(client);
+
+                // Xử lý client
+                handle_client(client);
             }
         }
-    
-    getchar();
-    close(listener);    
+        else if (pid < 0)
+        {
+            perror("Error forking");
+            exit(1);
+        }
+    }
 
-    killpg(0, SIGKILL);
+    // Tiến trình cha chờ tiến trình con kết thúc
+    for (i = 0; i < NUM_CHILDREN; i++)
+    {
+        wait(NULL);
+    }
+
+    // Đóng socket lắng nghe
+    close(listener);
 
     return 0;
 }
--- BTVN_2905/telnet/telnet_clientfork.c
+++ BTVN_2905/telnet/telnet_clientfork.c
@@ -8,29 +8,31 @@
 #include <signal.h>
 #include <sys/wait.h>
 
-int main() {
+int main()
+{
     int client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    addr.sin_port = htons(9000); 
+    addr.sin_port = htons(9000);
 
-    if (connect(client, (struct sockaddr *)&addr, sizeof(addr))) {
+    if (connect(client, (struct sockaddr *)&addr, sizeof(addr)))
+    {
         perror("connect() failed");
         return 1;
     }
-        
+
     // Tao them tien trinh moi, chay 2 cong viec song song
     if (fork() == 0)
     {
         // Tien trinh con
         // Doc du lieu tu ban phim, gui sang server
-        printf("child socket client = %d\n", client);
         char buf[256];
         while (1)
         {
             fgets(buf, sizeof(buf), stdin);
+            buf[strcspn(buf, "\n")] = '\0';
             send(client, buf, strlen(buf), 0);
             if (strncmp(buf, "exit", 4) == 0)
                 break;
@@ -40,7 +42,6 @@ int main() {
     {
         // Tien trinh cha
         // Nhan du lieu tu server va in ra man hinh
-        printf("main socket client = %d\n", client);
         char buf[256];
         while (1)
         {
@@ -48,7 +49,7 @@ int main() {
             if (ret <= 0)
                 break;
             buf[ret] = 0;
-            printf("Received: %s\n", buf);
+            printf("%s\n", buf);
         }
     }
 
